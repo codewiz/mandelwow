3180f30c5e35b34cc2e99f9b1e37da7eae56ed5c
Bernie Innocenti <bernie@codewiz.org>
2017-05-07 23:10:17 -0400

    Add a benchmark for ShadedCube.

--- benches/shaded_cube.rs
+++ benches/shaded_cube.rs
@@ -1,30 +1,29 @@
 #![feature(test)]
 
 extern crate mandelwow_lib;
-extern crate glutin;
 #[macro_use(uniform)]
 extern crate glium;
+extern crate glutin;
 extern crate test;
 
 use glium::DisplayBuild;
 use mandelwow_lib::Cube;
-use mandelwow_lib::bounding_box::*;
+use mandelwow_lib::shaded_cube::*;
 
 #[bench]
-fn bench_bounding_box(b: &mut test::Bencher) {
+fn bench_shaded_cube(b: &mut test::Bencher) {
     let display = glutin::WindowBuilder::new().build_glium().unwrap();
-    let program = solid_fill_program(&display);
+    let program = shaded_program(&display);
     let bounds = Cube { xmin: -2., xmax: 0.7, ymin: -1., ymax:  1., zmin: -1.1, zmax:  1.1 };
-    let bbox = BoundingBox::new(&display, &bounds, &program);
+    let cube = ShadedCube::new(&display, &bounds, &program);
     let mut frame = display.draw();
     b.iter(|| {
         let mat = [[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.0f32]];
         let uniforms = uniform! {
             model: mat,
-            view: mat,
-            perspective: mat,
+            viewpersp: mat,
         };
-        bbox.draw(&mut frame, &uniforms);
+        cube.draw(&mut frame, &uniforms);
     });
     frame.finish().unwrap();
 }
