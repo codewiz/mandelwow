aa2fc560e10dcd57f0d787d2ce55c97f5907c215
Bernie Innocenti <bernie@codewiz.org>
2017-06-27 02:23:15 -0400

    Update shaded_cube bench.

--- benches/shaded_cube.rs
+++ benches/shaded_cube.rs
@@ -7,15 +7,13 @@ extern crate glutin;
 extern crate test;
 
 use glium::DisplayBuild;
-use mandelwow_lib::Cube;
 use mandelwow_lib::shaded_cube::*;
 
 #[bench]
 fn bench_shaded_cube(b: &mut test::Bencher) {
     let display = glutin::WindowBuilder::new().build_glium().unwrap();
     let program = shaded_program(&display);
-    let bounds = Cube { xmin: -2., xmax: 0.7, ymin: -1., ymax:  1., zmin: -1.1, zmax:  1.1 };
-    let cube = ShadedCube::new(&display, &bounds, &program);
+    let cube = ShadedCube::new(&display, &program);
     let mut frame = display.draw();
     b.iter(|| {
         let model =     [[0.7, 0.5, -0.5, 0.0], [0.0, 0.7, 0.7, 0.0], [0.7, -0.5,  0.5,  0.0], [0., 0., -3.0, 1.0f32]];
