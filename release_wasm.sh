c616910b42c0511d231b1913859bc62ecf4d6652
Bernie Innocenti <bernie@codewiz.org>
2017-07-08 12:22:23 -0400

    Prevent global CFLAGS and CXXFLAGS from percolating into the wasm build.

--- release_wasm.sh
+++ release_wasm.sh
@@ -1,4 +1,6 @@
 set -e
+export CFLAGS="-O2"
+export CXXFLAGS="$CFLAGS"
 cargo rustc --target wasm32-unknown-emscripten --release --bin mandelwow -- -C link-args='-s USE_SDL=2 --preload-file flora.xm'
 cp -a target/wasm32-unknown-emscripten/release/mandelwow.js .
 cp -a target/wasm32-unknown-emscripten/release/deps/mandelwow-*.{wasm,data} .
