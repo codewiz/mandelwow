811e20651e7cc743ad654bda75aff82cae6d33b3
Bernie Innocenti <bernie@codewiz.org>
2017-09-09 14:10:32 -0400

    Improve asmjs build docs.

--- release_asmjs.sh
+++ release_asmjs.sh
@@ -1,4 +1,4 @@
 set -e
-cargo rustc --target asmjs-unknown-emscripten --release -- -C link-args='-s USE_SDL=2'
+cargo rustc --target asmjs-unknown-emscripten --release --no-default-features --bin mandelwow -- -C link-args='-s USE_SDL=2'
 cp -a target/asmjs-unknown-emscripten/release/mandelwow.js .
 emrun .
