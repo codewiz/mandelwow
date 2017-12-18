266501b90392c2f0f02292a6e7231b0974f371dc
Bernie Innocenti <bernie@codewiz.org>
2017-12-17 23:48:33 -0500

    Attempt at enabling WebGL2 in wasm (not yet working).

---
+++ debug_asmjs.sh
@@ -0,0 +1,4 @@
+set -e
+cargo rustc --target asmjs-unknown-emscripten --bin mandelwow -- -C link-args='--emrun -s USE_WEBGL2=1 -s USE_SDL=2 -s ASSERTIONS=2 --preload-file flora.xm'
+cp -a target/asmjs-unknown-emscripten/debug/mandelwow.js .
+emrun .
--- release_asmjs.sh
+++ release_asmjs.sh
@@ -1,4 +1,4 @@
 set -e
-cargo rustc --target asmjs-unknown-emscripten --release --no-default-features --bin mandelwow -- -C link-args='-s USE_SDL=2'
+cargo rustc --target asmjs-unknown-emscripten --release --no-default-features --bin mandelwow -- -C link-args='--emrun -s USE_WEBGL2=1 -s USE_SDL=2 -s ASSERTIONS=2 --preload-file flora.xm'
 cp -a target/asmjs-unknown-emscripten/release/mandelwow.js .
 emrun .
