3aaec48ccb98cb18f50898f5f76bd8cee19e8b90
Bernie Innocenti <codewiz@google.com>
2018-10-15 01:28:53 +0900

    Small tweaks to debug build scripts

--- debug_asmjs.sh
+++ debug_asmjs.sh
@@ -1,4 +1,6 @@
 set -e
+export CFLAGS="-g"
+export CXXFLAGS="$CFLAGS"
 cargo rustc --target asmjs-unknown-emscripten --bin mandelwow -- -C link-args='--emrun -s USE_WEBGL2=1 -s USE_SDL=2 -s ASSERTIONS=2 --preload-file flora.xm'
 cp -a target/asmjs-unknown-emscripten/debug/mandelwow.js .
 emrun .
--- debug_wasm.sh
+++ debug_wasm.sh
@@ -1,7 +1,7 @@
 set -e
 export CFLAGS="-g"
 export CXXFLAGS="$CFLAGS"
-cargo rustc --target wasm32-unknown-emscripten --bin mandelwow -- -C link-args='-s USE_SDL=2 --preload-file flora.xm'
+cargo rustc --target wasm32-unknown-emscripten --bin mandelwow -- -C link-args='--emrun -s USE_SDL=2 -s USE_WEBGL2=1 -s GL_ASSERTIONS=1 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1 --preload-file flora.xm'
 cp -a target/wasm32-unknown-emscripten/debug/mandelwow.{js,wasm} .
 cp -a target/wasm32-unknown-emscripten/debug/deps/mandelwow.data .
 emrun .
