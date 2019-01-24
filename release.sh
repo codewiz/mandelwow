80c5b26f6bb18d4f7eb0d75caaaf4906630db6de
Bernie Innocenti <codewiz@google.com>
2019-01-25 11:58:57 +1300

    Log binary size in release.sh to help debug bloat

--- release.sh
+++ release.sh
@@ -1,4 +1,14 @@
 set -e
 cargo build --release --no-default-features
 strip target/release/mandelwow
+
+(
+	echo "---------------------------------"
+	date
+	git branch -v
+	rustc --version
+	ls -la target/release/mandelwow
+	size --format=sysv target/release/mandelwow
+) | tee -a size.log
+
 upx -9 target/release/mandelwow
