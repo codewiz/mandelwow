db8dabba713ca281b3c8c49642eccf5a6f0af250
Bernie Innocenti <bernie@codewiz.org>
2020-04-01 00:42:57 +0900

    Remove deprecatd emscripten option ERROR_ON_MISSING_LIBRARIES

--- release_wasm.sh
+++ release_wasm.sh
@@ -2,7 +2,7 @@ set -e
 export CFLAGS="-O2"
 export CXXFLAGS="$CFLAGS"
 cargo rustc --target wasm32-unknown-emscripten --release --bin mandelwow -- \
-  -C link-args='--emrun -s USE_SDL=2 -s ERROR_ON_MISSING_LIBRARIES=0 -s ASSERTIONS=2 --preload-file flora.xm'
+  -C link-args='--emrun -s ASSERTIONS=2 --preload-file flora.xm'
 cp -a target/wasm32-unknown-emscripten/release/mandelwow.{js,wasm} .
 cp -a target/wasm32-unknown-emscripten/release/deps/mandelwow.data .
 emrun .
